44a5bd541696ba6ded1e32cd54c8fba47da3941a
Your Name <you@example.com>
2021-03-17 10:50:52 -0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { DataProfileComponent } from './components/data-profile/data-profile.component';
+import { FormationComponent } from './components/formation/formation.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { LanguageComponent } from './components/language/language.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    DataProfileComponent,
+    FormationComponent,
+    SkillComponent,
+    ExperienceComponent,
+    LanguageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/data-profile/data-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-profile',
+  templateUrl: './data-profile.component.html',
+  styleUrls: ['./data-profile.component.css']
+})
+export class DataProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
